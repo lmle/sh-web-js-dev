2dd9e36dacd0f28e025678163b90493c624b78c7
Ly Mai Le <lymaile@gmail.com>
2014-10-25 14:34:02 -0700

    (feat) Serves index.html

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-// Test
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var root = path.normalize(__dirname);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+app.route('/*').get(function(req, res) {
+  res.sendFile(root + '/index.html');
+});
+
+console.log('Listening on port', port);
